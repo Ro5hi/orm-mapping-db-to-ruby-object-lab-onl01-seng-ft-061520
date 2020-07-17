578f5c5a0ee9aa0cd7fceff54b9e12b3a1855935
Shirlen Detablan <slen.detablan@gmail.com>
2020-07-17 14:47:58 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -43,27 +43,16 @@ class Student
     .first
   end
   
-  # def self.students_below_12th_grade
-  #   sql = <<-SQL
-  #     SELECT * 
-  #     FROM students 
-  #     WHERE grade < 12 
-  #   SQL
+  def self.students_in_grade_9
+    sql = <<-SQL
+      SELECT * 
+      FROM students 
+      WHERE grade = 9; 
+    SQL
 
-  #   DB[:conn].execute(sql,name).map do |row|
-  #     self.new_from_db(row)
-  #   end 
-  
-  # def self.students_in_grade_9
-  #   sql = <<-SQL
-  #     SELECT * 
-  #     FROM students 
-  #     WHERE grade = 9; 
-  #   SQL
-
-  #   DB[:conn].execute(sql,name).map do |row|
-  #     self.new_from_db(row)
-  #   end 
+    DB[:conn].execute(sql,name).map do |row|
+      self.new_from_db(row)
+    end 
   
   # def self.students_in_grade_10(number)
   #   sql = <<-SQL
